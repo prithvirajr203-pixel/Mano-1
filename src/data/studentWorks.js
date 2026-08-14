46d31b964f02b9be06416333d990b89f0a1d07da
prithvirajr203-pixel <prithvirajr203@gmail.com>
2026-08-14 22:40:03 +0530

    Fix art form images on homepage - use Supabase URLs

--- src/data/studentWorks.js
+++ src/data/studentWorks.js
@@ -7,7 +7,6 @@ const studentWorks = [
     id: 1,title: "Pencil Portrait Study",category: "Drawing", image: "https://xebleaqktakzosahobxv.supabase.co/storage/v1/object/public/academy-images/student_works/f8c88e69-244a-4eef-bc05-e39277bdd346.jpg"},
 
   { id: 2, title: "Landscape in Watercolour", category: "Painting", image: "https://xebleaqktakzosahobxv.supabase.co/storage/v1/object/public/academy-images/student_works/0489ffc6-35e6-47b0-bce5-b1afeb325673.jpg" },
-  { id: 3, title: "Eye Study", category: "Sketching", image: "/images/student-works/work-3.jpg" },
   { id: 4, title: "Bird on a Branch", category: "Painting", image: "https://xebleaqktakzosahobxv.supabase.co/storage/v1/object/public/academy-images/student_works/4aa5d009-1cad-445a-8703-fdd84a36d6e1.jpg" },
   { id: 5, title: "Portrait in Colour", category: "Portrait", image: "/images/student-works/work-5.jpg" },
   { id: 6, title: "Street Sketch", category: "Sketching", image: "/images/student-works/work-6.jpg" },
