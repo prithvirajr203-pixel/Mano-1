953d0b38c720d14b26b9cd9fadd190830382645f
prithvirajr203-pixel <prithvirajr203@gmail.com>
2026-08-14 22:51:11 +0530

    Fix art form images on homepage - use Supabase URLs

--- src/data/studentWorks.js
+++ src/data/studentWorks.js
@@ -9,7 +9,7 @@ const studentWorks = [
   { id: 2, title: "Landscape in Watercolour", category: "Painting", image: "https://xebleaqktakzosahobxv.supabase.co/storage/v1/object/public/academy-images/student_works/0489ffc6-35e6-47b0-bce5-b1afeb325673.jpg" },
   { id: 4, title: "Bird on a Branch", category: "Painting", image: "https://xebleaqktakzosahobxv.supabase.co/storage/v1/object/public/academy-images/student_works/4aa5d009-1cad-445a-8703-fdd84a36d6e1.jpg" },
   { id: 7, title: "Acrylic Still Life", category: "Painting", image: "https://xebleaqktakzosahobxv.supabase.co/storage/v1/object/public/academy-images/student_works/xxxx.jpg " },
-  { id: 8, title: "Charcoal Portrait", category: "Drawing", image: "https://xebleaqktakzosahobxv.supabase.co/storage/v1/object/public/academy-images/art_forms/e09d3314-1985-4d42-b58a-539f003497e1.jpg" },
-];
 
+  { id: 8, title: "Oil Pastels", category: "Drawing", image: "https://xebleaqktakzosahobxv.supabase.co/storage/v1/object/public/academy-images/student_works/380fe845-2393-4241-94e5-1e044ab902aa.jpg" },
+];
 export default studentWorks;
